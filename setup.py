d4519f91d090a45a5e1a453ad5ef3089127ce2bf
bymoye <964907582@qq.com>
2023-04-16 11:40:44 +0800

    update: version

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     ),
     author="bymoye",
     author_email="s3moye@gmail.com",
-    version="0.0.5",
+    version="0.0.6",
     description="A fast random number generator for python",
     long_description=readme(),
     long_description_content_type="text/markdown",
